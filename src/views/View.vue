bea1337838ea1212aa1c9970cf6518526f8c00b8
sungsuhan <swhanssu@gmail.com>
2022-08-02 17:52:23 +0900

    first

---
+++ src/views/View.vue
@@ -0,0 +1,22 @@
+<template>
+ <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+
+<style>
+</style>
