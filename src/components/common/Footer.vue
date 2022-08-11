8871e27102936a9940b91722724bb421816cd456
sungsuhan <swhanssu@gmail.com>
2022-08-11 22:12:34 +0900

    footer

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,6 +1,28 @@
 <template>
   <footer>
-    <p>copyRight fyzh99.tistory.com</p>
+    <br /><br />
+    <span>이용약관 | </span>
+    <span>개인정보 처리방침 | </span>
+    <span>T우주 구독 서비스 이용약관 | </span>
+    <span>우주패스 상품약관</span>
+    <button style="margin-left: 200px" type="button" class="btn btn-secondary">English ></button>
+    <button style="margin-left: 30px" class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+      Family Site
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li><a class="dropdown-item" href="#">T-world Biz</a></li>
+      <li><a class="dropdown-item" href="#">SK Telecom</a></li>
+      <li><a class="dropdown-item" href="#">SK 브로드밴드</a></li>
+      <li><a class="dropdown-item" href="#">파트너온</a></li>
+      <li><a class="dropdown-item" href="#">행복스토어</a></li>
+    </ul>
+    <br /><br />
+    <hr width="1400px" style="margin-left: 250px"><br />
+    <div>
+      서울특별시 중구 을지로 65대표이사/사장 : 유영상사업자등록번호 : 104-81-37225판매허가번호 : 중구 02923호 사업자정보확인
+      고객센터 : 국번없이 1800-0515(유료/유선, 타사고객) 또는 1505(무료/SK텔레콤 휴대폰)
+      COPYRIGHT © SK TELECOM CO., LTD. ALL RIGHTS RESERVED.
+    </div><br /><br />
   </footer>
 </template>
 
@@ -11,5 +33,5 @@ export default {
 </script>
 
 <style scoped>
-footer{border-top:1px solid #35495e; text-align:center; font-size:16px; color:#41b883; margin:100px 0 0 0;}
+footer{border-top:1px solid #35495e; font-size:16px; color: #07170f; margin:100px 0 0 0;}
 </style>
